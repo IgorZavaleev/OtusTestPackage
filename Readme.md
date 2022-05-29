14072a19ef9cb480e418bcab91c8e284f82f23cd
Igor Zavaleev <zavaleev_i@utkonos.ru>
2022-05-29 16:34:11 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This component provides basic poem reading functionality.
---
+++ src/ReadPoem.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IgorZavaleev\OtusExercise;
+
+class ReadPoem
+{
+    public function dontBeShy() {
+        echo "To be, or not to be: that is the question.\n";
+    }
+}
